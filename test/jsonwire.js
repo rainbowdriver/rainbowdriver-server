f66d338e3f26fe5669e708ae5e13fa96b713e32a
Iraê <irae@irae.pro.br>
2012-11-30 20:46:27 -0200

    More tests for find element

--- test/jsonwire.js
+++ test/jsonwire.js
@@ -56,17 +56,32 @@ describe('JSON Wire API', function(){
                 done();
             });
         });
-        it('should look for element in browser', function(done){
+        it('should findElement an return ID for element in browser', function(done){
             var conn = new StubConnection();
             conn.message = JSON.stringify({
                 name: 'findElement',
-                status: 0
+                status: 0,
+                elementId: 'foo'
             });
             api.sessions.aaa = {
                 connection: conn
             };
             client.post('/wd/hub/session/aaa/element', { using: "css selector"}, function(err, req, res, obj) {
-                assert.deepEqual(obj, { name: 'findElement', sessionId: 'aaa', status: 0, value: {} });
+                assert.deepEqual(obj, { name: 'findElement', sessionId: 'aaa', status: 0, value: {"ELEMENT":"foo"} });
+                done();
+            });
+        });
+        it('proper response when element don\'t exist', function(done){
+            var conn = new StubConnection();
+            conn.message = JSON.stringify({
+                name: 'findElement',
+                status: 7
+            });
+            api.sessions.aaa = {
+                connection: conn
+            };
+            client.post('/wd/hub/session/aaa/element', { using: "css selector"}, function(err, req, res, obj) {
+                assert.deepEqual(obj, {"name":"findElement","sessionId":"aaa","status":7,"value":{}});
                 done();
             });
         });
@@ -87,7 +102,7 @@ util.inherits(StubConnection, events.EventEmitter);
 StubConnection.prototype.close = sinon.stub();
 StubConnection.prototype.write = function() {
     var that = this;
-    process.nextTick(function(){
+    process.nextTick(function(){ // necessary in case of write being called befor event binding
         that.emit('data', that.message);
     });
 };
