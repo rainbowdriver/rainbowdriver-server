c1d4bc970f1e0571624c33caf031c1e142c718a4
Iraê <irae@irae.pro.br>
2013-03-13 14:56:06 -0300

    Connection binding

--- src/browser.js
+++ src/browser.js
@@ -10,3 +10,22 @@ function Browser() {
 util.inherits(Browser, events.EventEmitter);
 
 module.exports = Browser;
+
+function _getConnection() {
+    return this._connection;
+}
+
+function _setConnection(connection) {
+    if (this._connection) {
+        throw "Could not override connection";
+    } else {
+        this._connection = connection;
+        this.emit('connected');
+    }
+}
+
+Object.defineProperty(Browser.prototype, 'connection', {
+    get: _getConnection,
+    set: _setConnection
+});
+
--- test/browser.test.js
+++ test/browser.test.js
@@ -1,25 +1,76 @@
 var assert = require('assert'),
     events = require('events'),
+    util = require('util');
     sinon = require('sinon'),
     Browser = require('../src/browser');
 
+function StubConnection(attrs) {
+    var attr;
+    events.EventEmitter.call(this);
+    for (attr in attrs) {
+        this[attr] = attrs[attr];
+    }
+    return this;
+}
+
+util.inherits(StubConnection, events.EventEmitter);
+
+StubConnection.prototype.close = function () {
+    this.emit('close');
+};
+
+
 describe('Browser', function(){
 
-    it('is an event emmiter', function(){
-        var browser = new Browser(),
-            stub = sinon.stub(),
-            expect = { a: 'b' };
+    describe('constructor', function() {
 
-        browser.on('randomEvent', stub);
-        browser.emit('randomEvent', expect);
+        it('should be an event emmiter', function(){
+            var browser = new Browser(),
+                stub = sinon.stub(),
+                expect = { a: 'b' };
 
-        assert(browser instanceof events.EventEmitter, 'instanceof EventEmitter');
-        assert(stub.calledWith(expect));
+            browser.on('randomEvent', stub);
+            browser.emit('randomEvent', expect);
+
+            assert(browser instanceof events.EventEmitter, 'instanceof EventEmitter');
+            assert(stub.calledWith(expect));
+        });
+
+        it('should have contructor safeguard', function(){
+            var browser = Browser();
+            assert(browser instanceof events.EventEmitter, 'instanceof EventEmitter');
+        });
+
     });
 
-    it('has contructor safeguard', function(){
-        var browser = Browser();
-        assert(browser instanceof events.EventEmitter, 'instanceof EventEmitter');
+    describe('connection binding', function() {
+        it('should save connection', function() {
+            var browser = Browser(),
+                fakeConn = new StubConnection();
+
+            browser.connection = fakeConn;
+            assert.equal(browser.connection, fakeConn);
+        });
+        it('should not accept a second connection', function() {
+            var browser = Browser(),
+                fakeConn1 = new StubConnection(),
+                fakeConn2 = new StubConnection();
+
+            browser.connection = fakeConn1;
+            assert.throws(function() {
+                browser.connection = fakeConn2;
+            });
+            assert.equal(browser.connection, fakeConn1);
+        });
+        it('should emit "connected" when connection is set', function(endTest) {
+            var browser = Browser(),
+                fakeConn = new StubConnection();
+
+            browser.on('connected', function() {
+                endTest();
+            });
+            browser.connection = fakeConn;
+        });
     });
 
 });
