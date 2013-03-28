99712039a80e693c31554577c53b809b9643189b
Iraê <irae@irae.pro.br>
2013-03-28 10:51:28 -0300

    Added suport to removeListeners; better logging

--- src/connection_manager.js
+++ src/connection_manager.js
@@ -2,32 +2,46 @@ var sockjs   = require('sockjs'),
     colorize = require('colorize'),
     Browser = require('../src/browser'),
     waitingConnections = [],
-    waitingListeners = [];
+    waitingListeners = [],
+    con_counter = 0,
+    lis_counter = 0;
 
 socket_server = sockjs.createServer();
 socket_server.on('connection', function(connection) {
-    colorize.console.log('#magenta[--- browser connected]');
     var browser = new Browser();
+    browser.con_counter = ++con_counter;
+    colorize.console.log('#magenta[--- browser ('+browser.con_counter+') connected]');
     browser.verbose = true;
     browser.connection = connection;
     browser.on('close', function() {
-        colorize.console.log('#magenta[--- browser disconnected]');
+        colorize.console.log('#magenta[--- browser ('+browser.con_counter+') disconnected]');
     });
-    listener = waitingListeners.shift();
+    if(browser.id) {
+        findListener(browser);
+    } else {
+        browser.on('ready', function() {
+            findListener(browser);
+        });
+    }
+});
+
+function findListener(browser) {
+    var listener = waitingListeners.shift();
     if(listener) {
         respondWithBrowser(browser, listener);
     } else {
         waitingConnections.push(browser);
     }
-});
+}
 
 function respondWithBrowser(browser, listener) {
-    colorize.console.log('#yellow[--- binded connections]');
+    colorize.console.log('#yellow[--- binded connections (listener '+listener.lis_counter+' and browser '+browser.con_counter+')]');
     listener(browser);
 }
 
-function getBrowser(receiver, filterFn) {
-    colorize.console.log('#magenta[--- driver connected]');
+function getBrowser(listener, filterFn) {
+    listener.lis_counter = ++lis_counter;
+    colorize.console.log('#magenta[--- driver ('+listener.lis_counter+') connected]');
     var connection,
         acceptableConnections = waitingConnections;
     if(filterFn) {
@@ -35,18 +49,26 @@ function getBrowser(receiver, filterFn) {
     }
     connection = acceptableConnections.shift();
     if(connection) {
-        respondWithBrowser(connection, receiver);
+        respondWithBrowser(connection, listener);
         var index = waitingConnections.indexOf(connection);
         if(index !== -1) {
             waitingConnections.splice(index, 1);
         }
     } else {
-        waitingListeners.push(receiver);
+        waitingListeners.push(listener);
+    }
+}
+
+function removeListener(listener) {
+    var index = waitingListeners.indexOf(listener);
+    if (index !== -1) {
+        waitingListeners.splice(index, 1);
     }
 }
 
 module.exports = {
     getBrowser: getBrowser,
+    removeListener: removeListener,
     waitingListeners: waitingListeners,
     waitingConnections: waitingConnections,
     socket_server: socket_server
