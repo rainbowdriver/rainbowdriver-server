3e2b18cc8134890c564056e928ed02cad1102fc7
Iraê <irae@irae.pro.br>
2013-04-04 17:43:29 -0300

    Move input to new dll; delete old to prevent usage

--- helpers/low_level_keyb.ps1
+++ helpers/low_level_keyb.ps1
@@ -1,10 +1,25 @@
 $scriptpath = $MyInvocation.MyCommand.Path
 $dir = Split-Path $scriptpath
 
-[Reflection.Assembly]::LoadFile("$dir/InputSimulator.dll")
+[Reflection.Assembly]::LoadFile("$dir/WindowsInput.dll")
+
+# Please, keep comments because this lib is not documented
+
+# Write-Host "MoveMouseToPositionOnVirtualDesktop"
+# $mouse = new-object WindowsInput.MouseSimulator
+# $mouse.MoveMouseToPositionOnVirtualDesktop(0,0)
+# $mouse.LeftButtonDown()
+# $mouse.LeftButtonUp()
+
+$keyb = new-object WindowsInput.KeyboardSimulator
 
 if ($args.length -eq 1) {
-	[WindowsInput.InputSimulator]::SimulateKeyPress($args[0])
+    Write-Host "Simulating KeyPress "
+    Write-Host $args[0]
+    $keyb.KeyPress($args[0])
 } else  {
-	[WindowsInput.InputSimulator]::SimulateModifiedKeyStroke($args[0], $args[1])
+    Write-Host "Simulating ModifiedKeyStroke"
+    Write-Host $args[0]
+    Write-Host $args[1]
+    $keyb.ModifiedKeyStroke($args[0], $args[1])
 }
